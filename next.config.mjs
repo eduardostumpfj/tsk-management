46780c3a8d03712efdce20324fc6b328c81e831e
eduardo <eduardostumpfj@gmail.com>
2025-01-20 22:17:02 -0300

    wip

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    exportTrailingSlash: true,
-    output: 'export',
-    distDir: 'dist',
+    trailingSlash: true,     // Make sure URLs end with a slash (important for GitHub Pages)
+    output: 'export',        // Enable static export
+    distDir: 'dist',         // Set the directory to 'dist'
     images: {
-        unoptimized: true,
+      unoptimized: true,     // Disable image optimization (optional, but helps for static exports)
     },
-};
-
-export default nextConfig;
+  };
+  
+  export default nextConfig;
