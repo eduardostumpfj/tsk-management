93f8b9780be7adc9e98e0ea6e3ddbf707bd7f5f2
Eduardo Stumpf Lima Junior <eduardo.sjunior@bomjesus.br>
2025-02-06 14:49:10 -0300

    wip

--- next.config.mjs
+++ next.config.mjs
@@ -1,11 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    trailingSlash: true,     // Make sure URLs end with a slash (important for GitHub Pages)
-    output: 'export',        // Enable static export
-    distDir: 'dist',         // Set the directory to 'dist'
-    images: {
-      unoptimized: true,     // Disable image optimization (optional, but helps for static exports)
-    },
+    output: 'exports',
   };
   
   export default nextConfig;
